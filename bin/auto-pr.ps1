409b94867642e3fd972be14160e3a86d4c1df3ee
Isaac Shoebottom <ir.shoebottom@gmail.com>
2024-03-07 16:47:04 -0400

    ci: :speech_balloon: Change upstream parameter in pr script
    Updated the PR script to link to my repo instead of the template repo

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "IsaacShoebottom/scoop-bucket:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
